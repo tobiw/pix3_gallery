ef9552c752febab690f86cac8002ff6101ea2a67
Tobi Wulff <tobi@swulff.de>
2018-11-12 23:33:29 +1300

    Use template.html (mostly placeholders)

--- pix3_gallery/config.py
+++ pix3_gallery/config.py
@@ -1,4 +1,5 @@
 config = {
+    'title': 'pix',
     'albums': {
         'sort': {
             'enable': True,
--- pix3_gallery/pix.py
+++ pix3_gallery/pix.py
@@ -1,4 +1,5 @@
 import os.path
+import time
 from .album import Album
 from .config import config
 
@@ -12,6 +13,23 @@ class Pix:
         self.root_album = Album(config['album_path'])
 
     def get_output(self):
+        with open('template.html', 'r') as f:
+            template = f.read()
+
+        time_start = time.time()
+
+        template = template.replace('@title@', config['title'])
+        template = template.replace('@breadcrumb@', '')
+        template = template.replace('@albums@', '')
+        template = template.replace('@pics@', '')
+        template = template.replace('@control@', '')
+        template = template.replace('@web-pic@', '')
+        template = template.replace('@comment@', '')
+        template = template.replace('@album-description@', '')
+        template = template.replace('@meta@', '')
+        template = template.replace('@gen-time@', str(time.time() - time_start))
+        return template
+
         r = ['<h2>Albums ({:d} items)</h2>'.format(len(self.root_album))]
         for a in self.root_album.albums:
             r.append('<li><a href="{:s}">{:s}</a></li>'.format(
