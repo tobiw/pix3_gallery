12a2342564e18833e66353014e9fe0b0a325143f
Tobi Wulff <tobi@swulff.de>
2018-11-12 16:35:10 +1300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,12 +5,18 @@ import setuptools
 with open('pix3_gallery/pix3_gallery.py', 'r') as f:
     version = str(ast.literal_eval(re.search(r'__version__\s=\s+(.*)', f.read()).group(1)))
 
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
 setuptools.setup(
     name='pix3_gallery',
     version=version,
+    description='Pix3 photo gallery',
+    long_description=long_description,
     maintainer='Tobi Wulff',
     maintainer_email='tobi@swulff.de',
-    url='http://www.github.com',
+    url='https://github.com/tobiw/pix3_gallery',
+    license='GPLv3',
     packages=['pix3_gallery'],
     install_requires=[],
     entry_points={
